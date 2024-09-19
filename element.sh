386e0a14f531abbd1baac57c651cfa46016f7b3c
Joshua McNeel <joshmcneel@gmail.com>
2024-09-19 16:46:24 +0000

    feat: finished queries for element

--- element.sh
+++ element.sh
@@ -18,9 +18,9 @@ else
   if [[ $USER_INPUT =~ ^[0-9]+$ ]]
   then
     # Query database using atomic_number and user input
-    FIND_ELEMENT_RESULT=$($PSQL "SELECT * FROM elements WHERE atomic_number = $USER_INPUT")
+    FIND_ELEMENT_RESULT=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $USER_INPUT")
   else 
-    FIND_ELEMENT_RESULT=$($PSQL "SELECT * FROM elements WHERE symbol = '$USER_INPUT' OR name = '$USER_INPUT'")
+    FIND_ELEMENT_RESULT=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$USER_INPUT' OR name = '$USER_INPUT'")
   fi
 
   # Check if input matches atomic_number, symbol, or name from database
@@ -29,7 +29,23 @@ else
     # If not found, error to user and exit
     echo -e "\nI could not find that element in the database.\n"
   else
-    # If found, print information to user
-    echo "Element found"
+    # If found, query for element name and symbol, and read the results into variables
+    ELEMENT=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE atomic_number = $FIND_ELEMENT_RESULT")
+    IFS="|" read ATOMIC_NUMBER SYMBOL NAME <<< "$ELEMENT"
+
+    # Query for the element's properties, and read the results into variables
+    PROPERTIES=$($PSQL "SELECT atomic_mass, melting_point_celsius, boiling_point_celsius, type_id FROM properties WHERE atomic_number = $FIND_ELEMENT_RESULT")
+    IFS="|" read ATOMIC_MASS MELTING_POINT_CELSIUS BOILING_POINT_CELSIUS TYPE_ID <<< "$PROPERTIES"
+
+    # Query for the element's type
+    TYPE=$($PSQL "SELECT type FROM types WHERE type_id = $TYPE_ID")
+
+    # Print result to user
+    echo $FIND_ELEMENT_RESULT
+    echo $ELEMENT
+    echo $PROPERTIES
+    echo $TYPE
+
+    echo -e "\nPLACEHOLDER STATEMENT: The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius.\n"
   fi
 fi
